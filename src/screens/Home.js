18cba4f2ce777444d2184aee5e39f96bb2329d47
DESKTOP-CU72R65\antod <antode@live.fr>
2021-11-09 18:00:40 +0100

    merge fail

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,18 @@
-import React, { useState, useCallback } from 'react';
-import PropTypes from 'prop-types';
-import { useAsyncEffect } from 'use-async-effect';
+import React, { useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import { useAsyncEffect } from "use-async-effect";
 import {
-  RefreshControl, SafeAreaView, ScrollView, StyleSheet,
-} from 'react-native';
-import {
-  Toast,
-} from 'native-base';
-import { postActions } from '../redux/actions';
-import Loading from '../components/Loading';
-import toasts from '../redux/helpers/toasts';
-import { Post } from '../components/home';
+  Dimensions,
+  View,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { Text, Toast, Box, Image } from "native-base";
+import { postActions } from "../redux/actions";
+import Loading from "../components/Loading";
+import toasts from "../redux/helpers/toasts";
 
 const styles = StyleSheet.create({
   container: {
@@ -18,8 +20,8 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
@@ -35,7 +37,8 @@ function HomeScreen() {
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
-    postActions.getPosts()
+    postActions
+      .getPosts()
       .then((data) => {
         setPosts(data);
       })
@@ -47,12 +50,15 @@ function HomeScreen() {
 
   useAsyncEffect(async (isMounted) => {
     if (isMounted()) {
-      postActions.getPosts().then((data) => {
-        setPosts(data);
-        if (isMounted()) setMounted(true);
-      }).catch(() => {
-        Toast.show(toasts.globalError);
-      });
+      postActions
+        .getPosts()
+        .then((data) => {
+          setPosts(data);
+          if (isMounted()) setMounted(true);
+        })
+        .catch(() => {
+          Toast.show(toasts.globalError);
+        });
     }
   }, []);
 
@@ -63,17 +69,61 @@ function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView
-        contentContainerStyle={styles.scrollView}
-        refreshControl={(
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-          />
-        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       >
-        {posts.map((post) => (
-          <Post post={post} />
-        ))}
+        {posts.map(
+          (post) => (
+            console.log("Dans post"),
+            console.log(post),
+            (
+              <Box key={post.id} width="100%">
+                <Box
+                  style={{
+                    flexDirection: "row",
+                    width: "100%",
+                    marginTop: "3%",
+                    marginBottom: "5%",
+                  }}
+                >
+                  <Image
+                    source={{
+                      uri:
+                        "https://epigrambe.herokuapp.com" +
+                        post.author.profile_picture,
+                    }}
+                    width="8%"
+                    height="100%"
+                    style={{
+                      marginLeft: "2%",
+                      borderRadius: 20,
+                      marginTop: "1%",
+                    }}
+                    alt="test"
+                  />
+                  <Text style={{ marginTop: "2%", marginLeft: "3%" }}>
+                    {post.author.username}
+                  </Text>
+                </Box>
+                <Image
+                  source={{ uri: post.image }}
+                  alt="Post Image"
+                  width={Dimensions.get('window').width}
+                  height={Dimensions.get('window').width}
+                />
+                <Box style={{ flexDirection: "row" }}>
+                  <Text style={{ marginTop: "2%", marginLeft: "3%" }}>
+                    {post.author.username}
+                  </Text>
+                  <Text style={{ marginTop: "2%", marginLeft: "3%" }}>
+                    {post.caption}
+                  </Text>
+                </Box>
+              </Box>
+            )
+          )
+        )}
       </ScrollView>
     </SafeAreaView>
   );
